ed69872c52274b3854c63a557848b118b788a967
Richard Hoffman <coverslide@gmail.com>
2013-03-01 14:02:53 -0800

    Set binaryType to ArrayBuffer, also reconnect automatically

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var msgpack = require('msgpack-js-browser')
 
 require('mkee')(FileSocketClient)
-require('mkee')(FileSocketReadStream)
+require('mkstream')(FileSocketReadStream)
 
 module.exports = FileSocketClient
 
@@ -11,26 +11,11 @@ var S   = 1
 , DIR   = S++
 , FILE  = S++
 
-function FileSocketClient(url){
+function FileSocketClient(url, options){
   var _this = Object.create(FileSocketClient.prototype)
 
   if(!url) url = 'ws://' + window.location.host
-  var sock = new WebSocket(url)
-  var requests = {}
-
-  sock.onmessage = messageHandler
-
-  sock.onerror = function(err){
-    _this.emit('error', err)
-  }
-
-  sock.onopen = function(e){
-    _this.emit('connect', e)
-  }
-
-  sock.onclose = function(e){
-    _this.emit('disconnect', e)
-  }
+  var sock, requests = {}
 
   _this.requestFile = function(path, cb){
     send({type:FILE, path:path}, cb)
@@ -39,45 +24,73 @@ function FileSocketClient(url){
   _this.requestDirectory = function(path, cb){
     send({type:DIR, path:path}, cb)
   }
+  
+  connect()
 
   return _this
 
   function messageHandler(message){
-    blobToArrayBuffer(message.data, function(err, ab){
-      if(err) return _this.emit('error', err)
-      var msg = msgpack.decode(ab)
-      var id = msg.id
-      var request = requests[id]
+    var msg = msgpack.decode(message.data)
+    var id = msg.id
+    var request = requests[id]
 
-      if(request){
-        var type = request.type
-        if(msg.error){
+    if(request){
+      var cb = request.cb
+      var type = request.type
+      if(msg.error){
+        requests[id] = null
+        if(cb) cb(new Error(msg.error))
+        else _this.emit('error', new Error(msg.error))
+      } else if(type == DIR){
+        requests[id] = null
+        if(cb) cb(null, msg.stats)
+        _this.emit('dir', request.path, msg.stats)
+      } else if(type == FILE){
+        if(msg.stat){
+          request.stream = new FileSocketReadStream()
+          if(cb) cb(null, msg.stat, request.stream)
+          _this.emit('file', request.path, msg.stat, request.stream)
+        } else if(msg.data){
+          request.stream.emit('data', msg.data)
+        }
+        if(msg.eof){
           requests[id] = null
-          _this.emit('error', new Error(msg.error))
-        } else if(type == DIR){
-          requests[id] = null
-          _this.emit('dir', request.path, msg.stat)
-        } else if(type == FILE){
-          if(msg.stat){
-            request.stream = new FileSocketReadStream()
-            _this.emit('file', request.path, msg.stat, request.stream)
-          } else if(msg.data){
-            request.stream.emit('data', msg.data)
-          }
-          if(msg.eof){
-            requests[id] = null
-            request.stream.emit('end')
-          }
-        } else {
-          requests[id] = null
-          _this.emit('error', new Error('Unknown type: ' + request.type + ' encountered'))
+          request.stream.emit('end')
         }
       } else {
-        _this.emit('error', new Error('ID ' + id + ' Not found'))
+        requests[id] = null
+        var error = new Error('Unknown type: ' + request.type + ' encountered')
+        if(cb) cb(error)
+        else _this.emit('error', error)
+
       }
-    })
+    } else {
+      _this.emit('error', new Error('ID ' + id + ' Not found'))
+    }
+  }
+
+  function connect(){
+    sock = new WebSocket(url)
+    sock.binaryType = 'arraybuffer'
+
+    sock.onmessage = messageHandler
+
+    sock.onerror = function(err){
+      _this.emit('error', err)
+    }
+
+    sock.onopen = function(e){
+      _this.emit('connect', e)
+    }
+
+    sock.onclose = function(e){
+      _this.emit('disconnect', e)
+      if(options.reconnect)
+        connect()
+    }
   }
   
+  //TODO: buffer commands when not connected
   function send(obj, cb){
     obj.id = genid()
     requests[obj.id] = obj
@@ -92,13 +105,5 @@ function genid(){
   return Math.random() * MAX_INT
 }
 
-function FileSocketReadStream(){}
-
-function blobToArrayBuffer(blob, cb){
-  var fr = new FileReader();
-  fr.onloadend = function(){
-    if(fr.error) return cb(fr.error)
-    else return cb(null, fr.result)
-  }
-  fr.readAsArrayBuffer(blob)
+function FileSocketReadStream(){
 }
